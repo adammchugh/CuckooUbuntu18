8169cbdae29412f10b04d3a36bc0b1fe6c4e4c67
adammchugh <34958726+adammchugh@users.noreply.github.com>
2019-04-28 19:53:32 +0930

    Update 03_config_cuckoo_vm.sh

--- 03_config_cuckoo_vm.sh
+++ 03_config_cuckoo_vm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Mount Windows installation media to the Ubuntu operating system. Press any key after drive has mounted."
+read
 vboxmanage startvm "cuckoo1"
 echo "Install and configure your Cuckoo VM to a ready state. Press any key to suspend and snapshot."
 read
