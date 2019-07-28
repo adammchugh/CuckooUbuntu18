6175028b3993b90fd011b6c447ec0855795c5daa
adammchugh <34958726+adammchugh@users.noreply.github.com>
2019-07-29 09:14:11 +0930

    Update 01_install_cuckoo.sh

--- 01_install_cuckoo.sh
+++ 01_install_cuckoo.sh
@@ -5,11 +5,9 @@ apt install python python-pip python-dev libffi-dev libssl-dev  -y
 apt install virtualbox virtualbox-guest-additions-iso virtualbox-dkms -y
 apt install libjpeg-dev zlib1g-dev swig ssdeep tcpdump mongodb volatility -y
 setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-usermod -a -G vboxusers cuckoo
 pip install -U weasyprint==0.42.2
 pip install -U cuckoo
 mkdir /etc/cuckoo
-chown cuckoo:root /etc/cuckoo
 chmod 750 /etc/cuckoo
 cuckoo --cwd /etc/cuckoo
 echo "export CUCKOO=/etc/cuckoo" >> ~/.bashrc
