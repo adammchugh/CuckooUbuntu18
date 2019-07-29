84a26e8f994e9e14f0c942b6856d81d8510e555a
adammchugh <34958726+adammchugh@users.noreply.github.com>
2019-07-29 09:43:25 +0930

    Update 03_config_cuckoo_vm.sh

--- 03_config_cuckoo_vm.sh
+++ 03_config_cuckoo_vm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "Mount Windows installation media to the Ubuntu operating system. Press any key after drive has mounted."
-read
-vboxmanage startvm "cuckoo1"
-echo "Install and configure your Cuckoo VM to a ready state. Press any key to suspend and snapshot."
-read
-vboxmanage snapshot "cuckoo1" take "snap_clean-state" --pause
-vboxmanage controlvm "cuckoo1" poweroff
-vboxmanage snapshot "cuckoo1" restorecurrent
+vboxmanage list vms
+echo -n "Enter name of VM [$vm] > "
+read vmname
+vboxmanage startvm $(vmname)
+
+echo vboxmanage snapshot "cuckoo1" take "snap_clean-state" --pause
+echo vboxmanage controlvm "cuckoo1" poweroff
+echo vboxmanage snapshot "cuckoo1" restorecurrent
