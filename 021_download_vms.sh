f1390e551ec4fa89f94aab3dfce9c632128db436
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-04-13 14:09:30 +0930

    Update 021_download_vms.sh

--- 021_download_vms.sh
+++ 021_download_vms.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 wget -O /opt/cuckoos/ovas/Windows10.zip https://az792536.vo.msecnd.net/vms/VMBuild_20190311/VirtualBox/MSEdge/MSEdge.Win10.VirtualBox.zip
 unzip /opt/cuckoos/ovas/Windows10.zip -d /opt/cuckoos/ovas/
+rm -f /opt/cuckoos/ovas/Windows10.zip
