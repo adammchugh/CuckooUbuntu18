7cfaac3ce4650b0bdfc94bc1a2d5cbf759c0c409
adammchugh <34958726+adammchugh@users.noreply.github.com>
2020-12-19 15:42:38 +1030

    Update 01_install_cuckoo.sh

--- 01_install_cuckoo.sh
+++ 01_install_cuckoo.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install python python-pip python-dev libffi-dev libssl-dev  -y
+sudo apt install python3 python3-pip python3-dev libffi-dev libssl-dev  -y
 sudo apt install virtualbox virtualbox-guest-additions-iso virtualbox-dkms -y
 sudo apt install libjpeg-dev zlib1g-dev swig ssdeep tcpdump mongodb volatility -y
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
-sudo pip install -U weasyprint==0.42.2
-sudo pip install -U cuckoo
+sudo pip3 install -U weasyprint==0.42.2
+sudo pip2 install -U pysistent==0.16.1
+sudo pip2 install -U cuckoo
 mkdir /etc/cuckoo
 chmod 750 /etc/cuckoo
 chmod -R $(whoami):$(whoami) /etc/cuckoo
