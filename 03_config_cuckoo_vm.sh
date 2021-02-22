6d253eefdab287d3f36daff3b31b2ae6408c3a44
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-02-22 21:19:20 +1030

    Delete 03_config_cuckoo_vm.sh

--- 03_config_cuckoo_vm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-vboxmanage list vms
-echo -n "Enter name of VM [$vm] > "
-read vmname
-vboxmanage startvm $(vmname)
-
-echo vboxmanage snapshot "cuckoo1" take "snap_clean-state" --pause
-echo vboxmanage controlvm "cuckoo1" poweroff
-echo vboxmanage snapshot "cuckoo1" restorecurrent
