361106aaf52cc072d392776adaa3e3fbfe074dff
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-02-22 21:19:03 +1030

    Delete 02_2_deploy_cuckoo_vm.sh

--- 02_2_deploy_cuckoo_vm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo vboxmanage import /opt/cuckoos/ovas/Windows10.ova --vsys 0 --vmname Windows10 --cpus 2 --memory 1024 --unit 10 --disk /opt/cuckoos/Windows10.vmdk
-vboxmanage modifyvm Windows10 --nic1 hostonly
-vboxmanage modifyvm Windows10 --hostonlyadapter1 vboxnet0
-vboxmanage sharedfolder add Windows10 --name "Shared" --hostpath /opt/cuckoos/shared --automount
