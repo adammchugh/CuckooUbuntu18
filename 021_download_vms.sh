7f37ba34122b439b85672523020436ad9243e6e1
adammchugh <34958726+adammchugh@users.noreply.github.com>
2021-02-22 21:18:54 +1030

    Delete 021_download_vms.sh

--- 021_download_vms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget -O /opt/cuckoos/ovas/Windows10.zip https://az792536.vo.msecnd.net/vms/VMBuild_20190311/VirtualBox/MSEdge/MSEdge.Win10.VirtualBox.zip
-unzip /opt/cuckoos/ovas/Windows10.zip -d /opt/cuckoos/ovas/
-mv /opt/cuckoos/ovas/MSEdge\ -\ Win10.ova /opt/cuckoos/ovas/Windows10.ova
-rm -f /opt/cuckoos/ovas/Windows10.zip
